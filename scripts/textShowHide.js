b1c30e0505b1398240df5a5a6ed982be1c218ebd
Alex Walton <alexjameswalton@gmail.com>
2021-06-10 00:58:08 +0530

    some updates

--- scripts/textShowHide.js
+++ scripts/textShowHide.js
@@ -1,59 +1,114 @@
 // Remove Image Script
 /*
 let myStoryNav = document.getElementById('myStoryNav');
-let alexsImage = document.getElementById('alexsImage');
-let aboutMeText = document.getElementById('aboutMeText');
-
-function hideImg() {
-  alexsImage.style.display = 'none';
-  aboutMeText.style.display = 'none';
-}
 
 myStoryNav.addEventListener('mousedown', hideImg);
-
-function showImg() {
-    alexsImage.style.display = 'block';
-    aboutMeText.style.display = 'inline';
-}
 
 myStoryNav.addEventListener('mouseup', showImg);
 */
 
+
+
+
 // Change Header Background Image When Hovering Over Nav Buttons
 let headerImage = document.getElementById('headerImage');
-let aboutMeText = document.getElementById('aboutMeText');
-let myBackgroundText = document.getElementById('myBackgroundText');
-let theFutureText = document.getElementById('theFutureText');
 let myStoryNav = document.getElementById('myStoryNav');
 let mySkillsNav = document.getElementById('mySkillsNav');
 let mySelfNav = document.getElementById('mySelfNav');
 
-function showMyStory() {
+function showMyStoryImage() {
   headerImage.style.backgroundImage = "url('images/headers/alberto-zanetti-I4WSyUKvJ2U-unsplash2.jpg')"
-  myBackgroundText.style.display = 'inline'
-}
-
-function hideMyStory() {
-  myBackgroundText.style.display = 'none'
-}
+};
 
 function showMySkillsImage() {
     headerImage.style.backgroundImage = "url('images/headers/jake-walker-MPKQiDpMyqU-unsplash2.jpg')"
-}
+};
+
+function hideMySkillsImage() {
+  headerImage.style.backgroundImage = "url('images/headers/alberto-zanetti-I4WSyUKvJ2U-unsplash2.jpg')"
+};
 
 function showMySelfImage() {
   headerImage.style.backgroundImage = "url('images/headers/mark-teachey-TfIQZ2gFEvI-unsplash.jpg')"
+};
+
+function hideMySelfImage() {
+  headerImage.style.backgroundImage = "url('images/headers/alberto-zanetti-I4WSyUKvJ2U-unsplash2.jpg')"
+};
+
+myStoryNav.addEventListener('mouseover', showMyStoryImage);
+mySkillsNav.addEventListener('mouseover', showMySkillsImage);
+mySkillsNav.addEventListener('mouseout', hideMySkillsImage);
+mySelfNav.addEventListener('mouseover', showMySelfImage);
+mySelfNav.addEventListener('mouseout', hideMySelfImage);
+
+
+// Show and hide text in each Box
+let aboutMeBox = document.getElementById('aboutMeBox');
+let myBackgroundBox = document.getElementById('myBackgroundBox');
+let theFutureBox = document.getElementById('theFutureBox');
+let myExperienceBox = document.getElementById('myExperienceBox');
+let mySkillsBox = document.getElementById('myExperienceBox');
+let mySelfBox = document.getElementById('mySelfBox');
+
+let aboutMeText = document.getElementById('aboutMeText');
+let alexsImage = document.getElementById('alexsImage');
+let myBackgroundText = document.getElementById('myBackgroundText');
+let theFutureText = document.getElementById('theFutureText');
+
+
+
+function showAlexsImg() {
+    alexsImage.style.display = 'block';
+    aboutMeText.style.display = 'inline';
+    aboutMeBox.style.justifyContent = 'flex-start'
 }
 
-myStoryNav.addEventListener('mouseover', showMyStory)
-myStoryNav.addEventListener('mouseout', hideMyStory)
-mySkillsNav.addEventListener('mouseover', showMySkillsImage)
-mySelfNav.addEventListener('mouseover', showMySelfImage)
+function hideAlexsImg() {
+    alexsImage.style.display = 'none';
+    aboutMeText.style.display = 'none';
+    aboutMeBox.style.justifyContent = 'center'
+}
+
+aboutMeBox.addEventListener('mouseover', showAlexsImg);
+aboutMeBox.addEventListener('mouseout', hideAlexsImg);
+
+
+function showMyBackgroundText () {
+    myBackgroundText.style.display = 'inline'
+};
+
+function hideMyBackgroundText () {
+    myBackgroundText.style.display = 'none'
+};
+
+myBackgroundBox.addEventListener('mouseover', showMyBackgroundText);
+myBackgroundBox.addEventListener('mouseout', hideMyBackgroundText);
+
+
+function showTheFutureText () {
+  theFutureText.style.display = 'inline'
+};
+
+function hideTheFutureText () {
+    theFutureText.style.display = 'none'
+};
+
+theFutureBox.addEventListener('mouseover', showTheFutureText);
+theFutureBox.addEventListener('mouseout', hideTheFutureText);
+
+
+
+
+
+
+// Bring My Skills to the top
+
 
 // beTheChange -> ifYouBuildItTheyWillCome
 
 let h1Element = document.getElementById("h1Element");
-let beTheChangeText = document.getElementById("beTheChangeText")
+let beTheChangeText = document.getElementById("beTheChangeText");
 
 function activateBeTheChange (){
   h1Element.innerHTML = `<span class="jt__row">
@@ -68,11 +123,11 @@ function activateBeTheChange (){
 <span class="jt__row jt__row--sibling" aria-hidden="true">
   <span class="jt__text">if You Build It, They Will Come</span>
 </span>`;
-}
+};
 
 function deactivateBeTheChange (){
   h1Element.innerHTML = `if You Build It, They Will Come`;
-}
+};
 
 beTheChangeText.onmouseover = activateBeTheChange;
 beTheChangeText.onmouseout = deactivateBeTheChange;
